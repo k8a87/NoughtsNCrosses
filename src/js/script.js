cd30d97983c7a6b3ad85371e5b224e10c815e94a
Keita Iisaka <iisak8a@gmail.com>
2022-10-18 05:52:55 +0100

    improved mobile responsivity

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,19 @@
-'use strict';
-const tl = document.querySelector('.tl'); //1
-const tm = document.querySelector('.tm'); //2
-const tr = document.querySelector('.tr'); //3
-const cl = document.querySelector('.cl'); //4
-const cm = document.querySelector('.cm'); //5
-const cr = document.querySelector('.cr'); //6
-const bl = document.querySelector('.bl'); //7
-const bm = document.querySelector('.bm'); //8
-const br = document.querySelector('.br'); //9
-const grid = document.querySelectorAll('.grid');
-const resetEl = document.querySelector('.reset');
-const iconEl = document.querySelectorAll('.iconStyle');
-const scoreOEl = document.querySelector('.scoreO');
-const scoreXEl = document.querySelector('.scoreX');
-const drawScoreEl = document.querySelector('.draw');
+"use strict";
+const tl = document.querySelector(".tl"); //1
+const tm = document.querySelector(".tm"); //2
+const tr = document.querySelector(".tr"); //3
+const cl = document.querySelector(".cl"); //4
+const cm = document.querySelector(".cm"); //5
+const cr = document.querySelector(".cr"); //6
+const bl = document.querySelector(".bl"); //7
+const bm = document.querySelector(".bm"); //8
+const br = document.querySelector(".br"); //9
+const square = document.querySelectorAll(".square");
+const resetEl = document.querySelector(".reset");
+const iconEl = document.querySelectorAll(".iconStyle");
+const scoreOEl = document.querySelector(".scoreO");
+const scoreXEl = document.querySelector(".scoreX");
+const drawScoreEl = document.querySelector(".draw");
 
 //Variables for start of game
 let activePlayer = 0;
@@ -24,22 +24,22 @@ let drawScore = 0;
 
 //Function to change background when won
 const winnerBackground = function (pos) {
-  pos.classList.remove('bg--circle', 'bg--cross', 'bg--grid');
-  pos.classList.add('bg--winner');
+  pos.classList.remove("bg--circle", "bg--cross", "bg--square");
+  pos.classList.add("bg--winner");
 };
 
 //Function when O wins
 const winnerO = () => {
   oScore++;
   scoreOEl.textContent = oScore;
-  resetEl.textContent = 'Player O wins!';
+  resetEl.textContent = "Player O wins!";
   gameActive = false;
 };
 //Function when X wins
 const winnerX = () => {
   xScore++;
   scoreXEl.textContent = xScore;
-  resetEl.textContent = 'Player X wins!';
+  resetEl.textContent = "Player X wins!";
   gameActive = false;
 };
 
@@ -51,28 +51,28 @@ const draw = () => {
   gameActive = false;
 };
 
-//For every grid element, check first if game is active and that there are no active squares (So you can't click on the same square twice). Then check which player is active.
-for (let i = 0; i < grid.length; i++) {
-  grid[i].addEventListener('click', function () {
+//For every square element, check first if game is active and that there are no active squares (So you can't click on the same square twice). Then check which player is active.
+for (let i = 0; i < square.length; i++) {
+  square[i].addEventListener("click", function () {
     if (
       gameActive &&
-      !grid[i].classList.contains('oWin') &&
-      !grid[i].classList.contains('xWin')
+      !square[i].classList.contains("oWin") &&
+      !square[i].classList.contains("xWin")
     ) {
       if (activePlayer === 0) {
-        iconEl[i].classList.add('far', 'fa-circle');
-        grid[i].classList.remove('bg--grid');
-        grid[i].classList.add('bg--circle');
-        grid[i].classList.add('oWin');
+        iconEl[i].classList.add("far", "fa-circle");
+        square[i].classList.remove("bg--square");
+        square[i].classList.add("bg--circle");
+        square[i].classList.add("oWin");
         resetEl.textContent = "Player X's turn";
 
         //Check to see if three O's in a row
 
         if (
           //top horizontal
-          tl.classList.contains('oWin') &&
-          tm.classList.contains('oWin') &&
-          tr.classList.contains('oWin')
+          tl.classList.contains("oWin") &&
+          tm.classList.contains("oWin") &&
+          tr.classList.contains("oWin")
         ) {
           winnerBackground(tl);
           winnerBackground(tm);
@@ -80,9 +80,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //center horizontal
-          cl.classList.contains('oWin') &&
-          cm.classList.contains('oWin') &&
-          cr.classList.contains('oWin')
+          cl.classList.contains("oWin") &&
+          cm.classList.contains("oWin") &&
+          cr.classList.contains("oWin")
         ) {
           winnerBackground(cl);
           winnerBackground(cm);
@@ -90,9 +90,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //bottom horizontal
-          bl.classList.contains('oWin') &&
-          bm.classList.contains('oWin') &&
-          br.classList.contains('oWin')
+          bl.classList.contains("oWin") &&
+          bm.classList.contains("oWin") &&
+          br.classList.contains("oWin")
         ) {
           winnerBackground(bl);
           winnerBackground(bm);
@@ -100,9 +100,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //left vertical
-          tl.classList.contains('oWin') &&
-          cl.classList.contains('oWin') &&
-          bl.classList.contains('oWin')
+          tl.classList.contains("oWin") &&
+          cl.classList.contains("oWin") &&
+          bl.classList.contains("oWin")
         ) {
           winnerBackground(tl);
           winnerBackground(cl);
@@ -110,9 +110,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //center vertical
-          tm.classList.contains('oWin') &&
-          cm.classList.contains('oWin') &&
-          bm.classList.contains('oWin')
+          tm.classList.contains("oWin") &&
+          cm.classList.contains("oWin") &&
+          bm.classList.contains("oWin")
         ) {
           winnerBackground(tm);
           winnerBackground(cm);
@@ -120,9 +120,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //right vertical
-          tr.classList.contains('oWin') &&
-          cr.classList.contains('oWin') &&
-          br.classList.contains('oWin')
+          tr.classList.contains("oWin") &&
+          cr.classList.contains("oWin") &&
+          br.classList.contains("oWin")
         ) {
           winnerBackground(tr);
           winnerBackground(cr);
@@ -130,9 +130,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //diagonal tl to br
-          tl.classList.contains('oWin') &&
-          cm.classList.contains('oWin') &&
-          br.classList.contains('oWin')
+          tl.classList.contains("oWin") &&
+          cm.classList.contains("oWin") &&
+          br.classList.contains("oWin")
         ) {
           winnerBackground(tl);
           winnerBackground(cm);
@@ -140,41 +140,41 @@ for (let i = 0; i < grid.length; i++) {
           winnerO();
         } else if (
           //diagonal bl to tr
-          bl.classList.contains('oWin') &&
-          cm.classList.contains('oWin') &&
-          tr.classList.contains('oWin')
+          bl.classList.contains("oWin") &&
+          cm.classList.contains("oWin") &&
+          tr.classList.contains("oWin")
         ) {
           winnerBackground(bl);
           winnerBackground(cm);
           winnerBackground(tr);
           winnerO();
         }
-        (tl.classList.contains('oWin') || tl.classList.contains('xWin')) &&
-        (tm.classList.contains('oWin') || tm.classList.contains('xWin')) &&
-        (tr.classList.contains('oWin') || tr.classList.contains('xWin')) &&
-        (cl.classList.contains('oWin') || cl.classList.contains('xWin')) &&
-        (cm.classList.contains('oWin') || cm.classList.contains('xWin')) &&
-        (cr.classList.contains('oWin') || cr.classList.contains('xWin')) &&
-        (bl.classList.contains('oWin') || bl.classList.contains('xWin')) &&
-        (bm.classList.contains('oWin') || bm.classList.contains('xWin')) &&
-        (br.classList.contains('oWin') || br.classList.contains('xWin')) &&
-        !grid[i].classList.contains('bg--winner')
+        (tl.classList.contains("oWin") || tl.classList.contains("xWin")) &&
+        (tm.classList.contains("oWin") || tm.classList.contains("xWin")) &&
+        (tr.classList.contains("oWin") || tr.classList.contains("xWin")) &&
+        (cl.classList.contains("oWin") || cl.classList.contains("xWin")) &&
+        (cm.classList.contains("oWin") || cm.classList.contains("xWin")) &&
+        (cr.classList.contains("oWin") || cr.classList.contains("xWin")) &&
+        (bl.classList.contains("oWin") || bl.classList.contains("xWin")) &&
+        (bm.classList.contains("oWin") || bm.classList.contains("xWin")) &&
+        (br.classList.contains("oWin") || br.classList.contains("xWin")) &&
+        !square[i].classList.contains("bg--winner")
           ? draw()
-          : console.log('Game on');
+          : console.log("Game on");
         //switch players
         activePlayer = activePlayer === 0 ? 1 : 0;
       } else if (activePlayer === 1) {
-        iconEl[i].classList.add('fas', 'fa-times');
-        grid[i].classList.remove('bg--grid');
-        grid[i].classList.add('bg--cross');
-        grid[i].classList.add('xWin');
+        iconEl[i].classList.add("fas", "fa-times");
+        square[i].classList.remove("bg--square");
+        square[i].classList.add("bg--cross");
+        square[i].classList.add("xWin");
         resetEl.textContent = "Player O's turn";
 
         if (
           //top horizontal
-          tl.classList.contains('xWin') &&
-          tm.classList.contains('xWin') &&
-          tr.classList.contains('xWin')
+          tl.classList.contains("xWin") &&
+          tm.classList.contains("xWin") &&
+          tr.classList.contains("xWin")
         ) {
           winnerBackground(tl);
           winnerBackground(tm);
@@ -182,9 +182,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //center horizontal
-          cl.classList.contains('xWin') &&
-          cm.classList.contains('xWin') &&
-          cr.classList.contains('xWin')
+          cl.classList.contains("xWin") &&
+          cm.classList.contains("xWin") &&
+          cr.classList.contains("xWin")
         ) {
           winnerBackground(cl);
           winnerBackground(cm);
@@ -192,9 +192,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //bottom horizontal
-          bl.classList.contains('xWin') &&
-          bm.classList.contains('xWin') &&
-          br.classList.contains('xWin')
+          bl.classList.contains("xWin") &&
+          bm.classList.contains("xWin") &&
+          br.classList.contains("xWin")
         ) {
           winnerBackground(bl);
           winnerBackground(bm);
@@ -202,9 +202,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //left vertical
-          tl.classList.contains('xWin') &&
-          cl.classList.contains('xWin') &&
-          bl.classList.contains('xWin')
+          tl.classList.contains("xWin") &&
+          cl.classList.contains("xWin") &&
+          bl.classList.contains("xWin")
         ) {
           winnerBackground(tl);
           winnerBackground(cl);
@@ -212,9 +212,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //center vertical
-          tm.classList.contains('xWin') &&
-          cm.classList.contains('xWin') &&
-          bm.classList.contains('xWin')
+          tm.classList.contains("xWin") &&
+          cm.classList.contains("xWin") &&
+          bm.classList.contains("xWin")
         ) {
           winnerBackground(tm);
           winnerBackground(cm);
@@ -222,9 +222,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //right vertical
-          tr.classList.contains('xWin') &&
-          cr.classList.contains('xWin') &&
-          br.classList.contains('xWin')
+          tr.classList.contains("xWin") &&
+          cr.classList.contains("xWin") &&
+          br.classList.contains("xWin")
         ) {
           winnerBackground(tr);
           winnerBackground(cr);
@@ -232,9 +232,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //diagonal tl to br
-          tl.classList.contains('xWin') &&
-          cm.classList.contains('xWin') &&
-          br.classList.contains('xWin')
+          tl.classList.contains("xWin") &&
+          cm.classList.contains("xWin") &&
+          br.classList.contains("xWin")
         ) {
           winnerBackground(tl);
           winnerBackground(cm);
@@ -242,9 +242,9 @@ for (let i = 0; i < grid.length; i++) {
           winnerX();
         } else if (
           //diagonal bl to tr
-          bl.classList.contains('xWin') &&
-          cm.classList.contains('xWin') &&
-          tr.classList.contains('xWin')
+          bl.classList.contains("xWin") &&
+          cm.classList.contains("xWin") &&
+          tr.classList.contains("xWin")
         ) {
           winnerBackground(bl);
           winnerBackground(cm);
@@ -261,7 +261,7 @@ for (let i = 0; i < grid.length; i++) {
         // (bl.classList.contains('oWin') || bl.classList.contains('xWin')) &&
         // (bm.classList.contains('oWin') || bm.classList.contains('xWin')) &&
         // (br.classList.contains('oWin') || br.classList.contains('xWin')) &&
-        // !grid[i].classList.contains('bg--winner')
+        // !square[i].classList.contains('bg--winner')
         //   ? draw()
         //   : console.log('Game on');
         activePlayer = activePlayer === 0 ? 1 : 0;
@@ -272,18 +272,18 @@ for (let i = 0; i < grid.length; i++) {
 // Reset
 const resetGame = () => {
   iconEl.forEach(i => {
-    i.classList.remove('far', 'fa-circle', 'fas', 'fa-times');
+    i.classList.remove("far", "fa-circle", "fas", "fa-times");
   });
-  grid.forEach(i => {
-    i.classList.remove('bg--circle', 'bg--cross', 'bg--winner', 'xWin', 'oWin');
-    i.classList.add('bg--grid');
+  square.forEach(i => {
+    i.classList.remove("bg--circle", "bg--cross", "bg--winner", "xWin", "oWin");
+    i.classList.add("bg--square");
   });
   gameActive = true;
   activePlayer = 0;
   resetEl.textContent = "Player O's turn";
 };
 
-resetEl.addEventListener('click', resetGame);
+resetEl.addEventListener("click", resetGame);
 
 // Draw;
 // MAKE A COUNTER FOR 9 SQUARES IF REACHED TO 0 THEN GAME OVER
@@ -296,13 +296,13 @@ resetEl.addEventListener('click', resetGame);
 // });
 
 // Change the background colour and text of Button on hover.
-resetEl.addEventListener('mouseover', function () {
-  resetEl.textContent = 'Clear board';
-  resetEl.style.backgroundColor = '#f7ff12';
+resetEl.addEventListener("mouseover", function () {
+  resetEl.textContent = "Clear board";
+  resetEl.style.backgroundColor = "#f7ff12";
 });
 // Only if the game is active, then the button will say who's turn it is.
-resetEl.addEventListener('mouseout', function () {
-  resetEl.style.backgroundColor = '#51ff00';
+resetEl.addEventListener("mouseout", function () {
+  resetEl.style.backgroundColor = "#51ff00";
   gameActive === false
     ? (resetEl.textContent = "Let's play again!")
     : activePlayer === 0
